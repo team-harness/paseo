783f43c58087d287b6d94ad8338bd9b82e981b29
Mohamed Boudra <boudra.moha@gmail.com>
2026-07-06 18:08:18 +0200

    Mark bundled-CLI daemons as desktop managed (#1919)

--- packages/desktop/bin/paseo.cmd
+++ packages/desktop/bin/paseo.cmd
@@ -11,5 +11,8 @@ if not exist "%APP_EXECUTABLE%" (
 
 set "ELECTRON_RUN_AS_NODE=1"
 set "PASEO_NODE_ENV=production"
+rem PASEO_DESKTOP_MANAGED marks daemons started through this bundled CLI as
+rem desktop-managed, so the desktop app restarts them when it upgrades.
+set "PASEO_DESKTOP_MANAGED=1"
 "%APP_EXECUTABLE%" --disable-warning=DEP0040 "%RESOURCES_DIR%\app.asar.unpacked\dist\daemon\node-entrypoint-runner.js" node-script "%RESOURCES_DIR%\app.asar\node_modules\@getpaseo\cli\dist\index.js" %*
 exit /b %errorlevel%
